3b7a56dac126d1cad16932f7c6df5ea7d9c2c56b
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-10-15 20:54:36 +0200

    Add check if build folder is ignored by git

--- generate_build/pitchfork.py
+++ generate_build/pitchfork.py
@@ -1,3 +1,5 @@
+import re
+import click
 from os import path
 
 
@@ -5,7 +7,14 @@ def _check_if_build_folder_is_ignored(project_root, out=None):
   gitignore_path=path.join(project_root, ".gitignore")
   if not path.exists(gitignore_path):
     if out:
-      out.warning("No .gitignore in project root. You should probably create one.")
+      out.warning("No {} in project root. You should create one.".format(click.style(".gitignore", fg="cyan")))
+    return False
+  with open(gitignore_path, 'r') as gitignore_file:
+    content = gitignore_file.read()
+    m = re.search('^build/?$', content, flags=re.MULTILINE)
+    if not m:
+      if out:
+        out.warning("Your build folder {} should be ignored by git. Add it to {}.".format(click.style("build/", fg="cyan"), click.style(".gitignore", fg="cyan")))
       return False
   return True
 
