7c6ea4b8f9f4d648ae1fe5b24bcd922d039b5a6c
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-06 22:55:49 +0100

    Refactor command execution
    Instead of buffering `stdout` and `stderr` of a subprocess running
    a command, redirect directly to parent's streams. This approach not
    only better works in terms of latency, but also better handles
    situations if child process is using ANSI colors, or carrige returns
    for fancy printing.

--- core/exec.go
+++ core/exec.go
@@ -2,8 +2,7 @@ package core
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
+	"os"
 	"os/exec"
 
 	"github.com/evilsocket/islazy/log"
@@ -19,62 +18,22 @@ const (
 )
 
 func PrintCommand(command []string, isDebug bool) error {
-	stdout, stderr, err := RunCommand(command)
-
-	if isDebug {
-		if len(stderr) > 0 {
-			fmt.Println(stderr)
-		}
-		if len(stdout) > 0 {
-			fmt.Println(stdout)
-		}
-	}
-
-	if err != nil {
-		if isDebug == false && len(stderr) > 0 {
-			fmt.Println(stderr)
-		}
-		return err
-	}
-
-	return nil
+	return RunCommand(command)
 }
 
-func RunCommand(command []string) (string, string, error) {
-	var (
-		stdout io.ReadCloser
-		stderr io.ReadCloser
-		err    error
-	)
-
+func RunCommand(command []string) error {
 	if len(command) < 1 {
-		return "", "", fmt.Errorf(errorNoCommand)
+		return fmt.Errorf(errorNoCommand)
 	}
 	log.Debug(debugExecutingCommand, command)
 
 	c := exec.Command(command[0], command[1:]...)
-
-	if stderr, err = c.StderrPipe(); err != nil {
-		log.Fatal(fatalFailedToGetStderrPipe)
-		return "", "", err
-	}
-	if stdout, err = c.StdoutPipe(); err != nil {
-		log.Fatal(fatalFailedToGetStdoutPipe)
-		return "", "", err
-	}
-
-	if err = c.Start(); err != nil {
-		log.Fatal(fatalFailedToStartCommand)
-		return "", "", err
-	}
-
-	stderrOut, _ := ioutil.ReadAll(stderr)
-	stdoutOut, _ := ioutil.ReadAll(stdout)
-
-	if err = c.Wait(); err != nil {
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
 		log.Fatal(fatalCommandFailedToRun)
-		return string(stdoutOut), string(stderrOut), err
+		return err
 	}
 
-	return string(stdoutOut), string(stderrOut), nil
+	return nil
 }
