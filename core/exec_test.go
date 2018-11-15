336376fefe0fe5a703b74baba09378ca8009a44c
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-11-16 00:41:03 +0100

    Add more exec tests

--- core/exec_test.go
+++ core/exec_test.go
@@ -3,10 +3,20 @@ package core
 import (
 	"os/exec"
 	"testing"
+
+	"github.com/evilsocket/islazy/log"
 )
 
+func init() {
+	log.Level = log.FATAL + 1
+}
+
 func fakeExecCommand(command string, args ...string) *exec.Cmd {
-	return exec.Command("go")
+	return exec.Command("go", "version")
+}
+
+func fakeExecFailedCommand(command string, args ...string) *exec.Cmd {
+	return exec.Command("go_")
 }
 
 func TestRunCommandWithNoCommandFails(t *testing.T) {
@@ -14,6 +24,24 @@ func TestRunCommandWithNoCommandFails(t *testing.T) {
 	defer func() { execCommand = exec.Command }()
 
 	if err := runCommand(nil, false); err == nil {
+		t.Errorf("Expected error, got %q", err)
+	}
+}
+
+func TestRunCommandThatExists(t *testing.T) {
+	execCommand = fakeExecCommand
+	defer func() { execCommand = exec.Command }()
+
+	if err := runCommand([]string{"test"}, false); err != nil {
+		t.Errorf("Expected no error, got %q", err)
+	}
+}
+
+func TestRunCommandThatFails(t *testing.T) {
+	execCommand = fakeExecFailedCommand
+	defer func() { execCommand = exec.Command }()
+
+	if err := runCommand([]string{"test"}, false); err == nil {
 		t.Errorf("Expected error, got %q", err)
 	}
 }
