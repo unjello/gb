a2551d4ef4b34e266e31ba973b7545ecb29fe656
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-12-03 06:11:25 -0600

    Add way to specify C++ features.
    First feature half-implemented is concepts. It's because it relies on
    G++-specific flag `-fconcepts` for now. Later, this should all be
    refactored to make use of `findc` command and shift reponsibility for
    comming up with a commandline on it.

--- core/options.go
+++ core/options.go
@@ -10,6 +10,18 @@ func getStandard(options map[string]interface{}) string {
 	}
 }
 
+func getConcepts(options map[string]interface{}) string {
+	if features, ok := options["features"]; ok == true {
+		for _, v := range features.([]interface{}) {
+			if v.(string) == "concepts" {
+				return "-fconcepts "
+			}
+		}
+	}
+
+	return ""
+}
+
 func GetCxxOptions(options map[string]interface{}) string {
-	return fmt.Sprintf("-Wall -Werror -std=%s", getStandard(options))
+	return fmt.Sprintf("-Wall -Werror -std=%s %s", getStandard(options), getConcepts(options))
 }
